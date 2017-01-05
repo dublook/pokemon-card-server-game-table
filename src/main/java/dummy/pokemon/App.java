42f738cd1e948cef3e6979fd8a54061f7d6bdd6e
dublook <kota.futami@gmail.com>
2017-01-06 00:48:06 +0900

    Get deck template list and kusa deck cards

--- src/main/java/dummy/pokemon/App.java
+++ src/main/java/dummy/pokemon/App.java
@@ -1,16 +1,12 @@
 package dummy.pokemon;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @EnableAutoConfiguration
@@ -33,4 +29,91 @@ public class App {
 				10002, 10002, 10002, 10003, 10003, 10003, 10003, 10003, 10003, 10003, 10003, 10003, 10004, 10004, 10004,
 				10004, 10004, 10004, 10011, 10011, 10011);
 	}
+
+    @RequestMapping(value = "/api/v1/deck/templates", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Map<String, String>> templates() {
+        List<Map<String, String>> templates = new ArrayList<>();
+        templates.add(createTemplateMaster("kusa", "草ポケモンデッキ"));
+        return templates;
+    }
+
+    @RequestMapping(value = "/api/v1/deck/templates/{deckCode}/cards", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Integer> templateCards(@PathVariable String deckCode) {
+        switch (deckCode) {
+            case "kusa":
+                return Arrays.asList(1,
+                        1,
+                        1,
+                        2,
+                        2,
+                        3,
+                        13,
+                        13,
+                        13,
+                        14,
+                        14,
+                        15,
+                        109,
+                        109,
+                        114,
+                        114,
+                        1001,
+                        1002,
+                        1003,
+                        1013,
+                        1013,
+                        1015,
+                        1015,
+                        1015,
+                        1016,
+                        1016,
+                        1019,
+                        1020,
+                        1020,
+                        8001,
+                        8001,
+                        8002,
+                        8002,
+                        8004,
+                        8006,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10001,
+                        10011,
+                        10011,
+                        10011);
+            default:
+                throw new IllegalArgumentException("No card set are found for deckCode=" + deckCode);
+        }
+    }
+
+    private Map<String, String> createTemplateMaster(String deckCode, String deckName) {
+        // define pojo for deck master model
+        Map<String, String> deckMst = new HashMap<>();
+        deckMst.put("deckCode", deckCode);
+        deckMst.put("deckName", deckName);
+        return deckMst;
+    }
+
 }
